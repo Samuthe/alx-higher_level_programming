ceb747a0dc3e6c3c14f45334269ff4a34c108395
Samuthe <ekatisamuel@gmail.com>
2022-07-06 15:33:20 +0300

    1. Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,4 +3,4 @@ def search_replace(my_list, search, replace):
     dup_list = my_list.copy()
     for i in range(len(dup_list)):
         dup_list[i] = replace if my_list[i] == search else my_list[i]
-        return dup_list
+    return dup_list
