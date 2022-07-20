bb6c39b565ee5b1341ab1358be35a76bddf370b9
Samuthe <ekatisamuel@gmail.com>
2022-07-20 09:26:26 +0300

    dealing with classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Square class 
+"""Square class
 definition"""
 
 
 class Square:
     """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
+Attributes:
+___size (int): size of a side of the square
+"""
     def __init__(self, size):
         """Initializes a square
-        Args:
-            size (int): size of a side of the square
-        Returns: None
-        """
+Args:
+size (int): size of a side of the square
+Returns: None
+"""
         self.__size = size
