1577f113959a35fbd1c04695555586a7aa93c8f3
Samuthe <ekatisamuel@gmail.com>
2022-07-22 16:22:21 +0300

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_identation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" The 5-text_indentation module"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after each of these characters: '.\
+            ', '?', ':'
+        Args:
+            text (str): Text to change
+        Returns:
+            the changed text
+    """
+    if text == "\n":
+        print()
+    if text is None:
+        raise TypeError("text must be a string")
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    delims = ".?:"
+    nen = ""
+    for i in text:
+        nen += i
+        if i in delims:
+            print(nen.strip())
+            print()
+            nen = ""
+    print(nen.strip(), end="")
