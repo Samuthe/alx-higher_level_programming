f9cbe2c35c668c96eecc33acf13eaf070de79f25
Samuthe <ekatisamuel@gmail.com>
2022-07-26 13:52:31 +0300

    area of Rect

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""create Rectangle class"""
+"""
+New class Rectangle
+"""
+
+
 class Rectangle:
+    """ Defines a rectangle """
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
+    """ WIDTH """
     @property
-
     def width(self):
         return (self.__width)
-    
+
     @width.setter
     def width(self, value):
         if type(value) != int:
@@ -18,25 +24,28 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    """ HEIGHT """
     @property
-
     def height(self):
         return (self.__height)
 
     @height.setter
     def height(self, value):
         if type(value) != int:
-            raise("height must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
+    """ Rectangle area """
     def area(self):
-        return (self.__width * self.__height)
+        return (self.__height * self.__width)
 
+    """ Rectangle perimeter """
     def perimeter(self):
         if self.width == 0:
-            return 0
+            return (0)
         if self.height == 0:
-            return 0
-        return (2 * (self.__width + self.__height))
+            return (0)
+        return (2 * (self.__height + self.__width))
+    
