97059399d495bc6015f1e0802d46c88df32f4e09
Samuthe <ekatisamuel@gmail.com>
2022-07-28 10:35:03 +0300

    strings

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def magic_string(count=[-1]):
-    count[0] += 1
-    return "Holberton" + ", Holberton" * count[0]
+
+def magic_string(x=[-1]):
+    x[0] += 1
+    return "Samuel" + ", Ekati" * x[0]
