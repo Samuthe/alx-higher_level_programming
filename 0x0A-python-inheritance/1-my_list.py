a5394ebe4e6c217d384e3b0aa4742369ee87f4e7
Samuthe <ekatisamuel@gmail.com>
2022-08-02 09:33:51 +0300

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,10 @@
-class MyList():
-    def print_sorted(self):
-        """prints a sorted list"""
-        print(sorted(self))
+#!/usr/bin/python3
+"""  New class """
 
+
+class MyList(list):
+    """ Mylist class that inherist from list """
+
+    def print_sorted(self):
+        """ Fucntion that prints a sorted list """
+        print(sorted(self))
