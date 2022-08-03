ec55dd872ad94f14c24086451a27ab8fa830d708
Samuthe <ekatisamuel@gmail.com>
2022-08-03 12:00:42 +0300

    writing

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,6 @@ and returns the number of characters written"""
 
 
 def write_file(filename="", text=""):
+    """writing into a file stdout"""
     with open(filename, "w", encoding="utf-8") as f:
         print(f.write(text))
