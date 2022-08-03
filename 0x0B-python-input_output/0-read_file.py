eba544d66d158682c8442992a2f5dad21546e7da
Samuthe <ekatisamuel@gmail.com>
2022-08-03 11:40:04 +0300

    readfile

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""A function that reads and outputs to stdout"""
+""" Function that reads a text file (UTF8) and prints it to stdout """
 
 
 def read_file(filename=""):
-    with open(filename, encoding="UTF-8") as f:
+    """ Reads UFT-8 File """
+    with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
