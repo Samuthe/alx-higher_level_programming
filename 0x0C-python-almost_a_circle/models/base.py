eeffaaacdf0f52978638b5b33fba0773d282a2c4
Samuthe <ekatisamuel@gmail.com>
2022-08-05 22:11:57 +0300

    circle project

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+class Base:
+    """New class"""
+
+
+"""private class attr"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """if id is not None, assign the public instance 
+        attribute id with this argument value"""
+        if id is not None:
+            self.id = id
+
+            """otherwise, increment __nb_objects and assign the 
+            new value to the public instance attribute id"""
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
