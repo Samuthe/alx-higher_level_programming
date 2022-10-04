e56d0c726dbc88705e277d4b6c7ce243848331c8
Samuthe <ekatisamuel@gmail.com>
2022-10-04 18:54:46 +0300

    factorizing

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,20 @@
 #!/usr/bin/node
 
-(function () {
+// (function () {
+//   if (process.argv.length <= 3) {
+//     console.log(0);
+//   } else {
+//     const sorting = process.argv.sort();
+//     console.log(sorting.reverse()[1]);
+//   }
+// })();
+
+function factorization () {
   if (process.argv.length <= 3) {
     console.log(0);
   } else {
     const sorting = process.argv.sort();
     console.log(sorting.reverse()[1]);
   }
-})();
+}
+factorization();
