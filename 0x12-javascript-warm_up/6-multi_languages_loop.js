c7ff63c0bd5c6600d506c538944e8549ae662f75
Samuthe <ekatisamuel@gmail.com>
2022-10-04 14:06:15 +0300

    arg

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+(function () {
+  const myList = ['C is fun', 'Python is cool', 'JavaScript is cool'];
+  for (let i = 0; i < myList.length; i++) {
+    console.log(myList[i]);
+  }
+})();
