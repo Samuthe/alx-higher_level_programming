c577033383ec01d62c7f1410694029a011ddcb5b
Samuthe <ekatisamuel@gmail.com>
2022-10-04 12:22:26 +0300

    display lines

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,13 +2,10 @@
 
 (function () {
   if (process.argv.length === 2) {
-    console.log('Argument found');
-
-  } else if (process.argv.length === 3) {
-    console.log('Arguments found');
-
-  } else {
     console.log('No argument');
+  } else if (process.argv.length === 3) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
   }
-  
 })();
