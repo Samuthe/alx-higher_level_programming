c06fd2a139115c48c615c2d59e27748fbb48e209
Samuthe <ekatisamuel@gmail.com>
2022-10-05 11:58:35 +0300

    add

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,15 +1,5 @@
 #!/usr/bi/node
 
-// function add (a, b) {
-//   return a + b;
-// }
-// exports.add = add;
-
 exports.add = function (a, b) {
   return a + b;
 };
-
-
-// exports.add = function (a, b) {
-//   return a + b;
-// };
