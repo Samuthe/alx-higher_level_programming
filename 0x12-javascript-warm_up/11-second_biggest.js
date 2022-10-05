77afd382515e62ec3a49bfe39a1274adf07aeeed
Samuthe <ekatisamuel@gmail.com>
2022-10-05 10:29:08 +0300

    sorting

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -19,9 +19,16 @@
 // }
 // factorization();
 
-if (process.argv.length <= 3) {
+// if (process.argv.length <= 3) {
+//   console.log(0);
+// } else {
+//   // const sorting = process.argv.sort();
+//   console.log(process.argv.sort().reverse()[1]);
+// }
+
+const mySol = process.argv;
+if (mySol.length <= 3) {
   console.log(0);
 } else {
-  // const sorting = process.argv.sort();
-  console.log(process.argv.sort().reverse()[1]);
+  console.log(mySol.sort((x, y) => y - x).slice(3)[0]);
 }
