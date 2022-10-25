8a78355affa1fcc292afce79374519f9311105c3
Samuthe <ekatisamuel@gmail.com>
2022-10-25 09:28:05 +0300

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,7 +11,3 @@ def fetcher():
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode("utf-8")))
-
-
-if __name__ == "__main__":
-    fetcher()
