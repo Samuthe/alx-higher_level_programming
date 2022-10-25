4629f1b67d66ecb3d2cf502c96dff379d689bcba
Samuthe <ekatisamuel@gmail.com>
2022-10-25 09:53:49 +0300

    github ids

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""Check status"""
-import requests
+"""Uses the GitHub API to display a GitHub ID based on given credentials.
+Usage: ./10-my_github.py <GitHub username> <GitHub password>
+  - Uses Basic Authentication to access the ID.
+"""
 import sys
-
-
-def searchapi():
-    """status"""
-    result = requests.get("https://swapi.co/api/people",
-                          params={"search": sys.argv[1]})
-
-    try:
-        data = result.json()
-        print("Number of results: {}".format(data["count"]))
-        for i in data["results"]:
-            print(i["name"])
-    except:
-        print("Not a valid JSON")
+import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    searchapi()
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
