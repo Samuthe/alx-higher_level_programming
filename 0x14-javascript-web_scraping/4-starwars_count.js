77e9cda6fc45073acd727b34e4e44e1882eb7b26
Samuthe <ekatisamuel@gmail.com>
2022-11-15 15:55:27 +0300

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+
+request(url, (error, response, body) => {
+  if (error) return console.log('error', error);
+  const data = JSON.parse(body).results;
+
+  for (const x in data) {
+    for (const value of data[x].characters) {
+      if (value.includes('18')) count++;
+    }
+  }
+  return console.log(count);
+});
