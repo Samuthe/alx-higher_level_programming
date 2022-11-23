d9e831e483c4b4e46887e699f221ab727282b7bc
Samuthe <ekatisamuel@gmail.com>
2022-11-23 14:23:14 +0300

    and press enter

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+  $('INPUT#btn_translate').click(() => {
+    const helloTranslate = $('INPUT#language_code').val();
+    const url = 'https://fourtonfish.com/hellosalut/?lang=' + helloTranslate;
+    $.get(url, function (data, textStatus) {
+      console.log(url, data, textStatus);
+      if (data.code !== 'none') {
+        $('DIV#hello').text(data.hello);
+      } else {
+        $('DIV#hello').text('Invalid Language code');
+      }
+    });
+  });
+  $('INPUT#language_code').keypress(function (event) {
+    if (event.which === 13) {
+      const helloTranslate = $('INPUT#language_code').val();
+      const url = 'https://fourtonfish.com/hellosalut/?lang=' + helloTranslate;
+      $.get(url, function (data, textStatus) {
+        console.log(url, data, textStatus);
+        if (data.code !== 'none') {
+          $('DIV#hello').text(data.hello);
+        } else {
+          $('DIV#hello').text('Invalid Language code');
+        }
+      });
+    }
+  });
+});
